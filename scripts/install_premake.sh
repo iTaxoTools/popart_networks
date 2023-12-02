fe2a47f1639be0a0b7eef1b5b2cdc7d5b1d78b0f
StefanPatman <stefanpatman91@gmail.com>
2023-12-02 21:58:09 +0200

    Add script for installing premake on unix

---
+++ scripts/install_premake.sh
@@ -0,0 +1,5 @@
+yum install -y wget unzip
+mkdir /tmp/premake
+wget https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-linux.tar.gz
+tar -xzvf premake-5.0.0-beta2-linux.tar.gz -C /tmp/premake
+export PATH=$PATH:/tmp/premake
