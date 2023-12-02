a0378ee3f5db30649159ddf2d18daa396475e808
StefanPatman <stefanpatman91@gmail.com>
2023-12-02 22:14:50 +0200

    Debug premake installation

--- scripts/install_premake.sh
+++ scripts/install_premake.sh
@@ -3,3 +3,5 @@ mkdir /tmp/premake
 wget https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-linux.tar.gz
 tar -xzvf premake-5.0.0-beta2-linux.tar.gz -C /tmp/premake
 export PATH=$PATH:/tmp/premake
+ls -al /tmp/premake
+ls -al .
