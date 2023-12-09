61c6162a2dd8c5b37bee7af60fbdb6a9ccb3c5c1
StefanPatman <stefanpatman91@gmail.com>
2023-12-09 21:07:46 +0200

    Remove premake related scripts

--- scripts/install_premake.sh
+++
@@ -1,7 +0,0 @@
-yum install -y wget unzip
-mkdir /tmp/premake
-wget https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-linux.tar.gz
-tar -xzvf premake-5.0.0-beta2-linux.tar.gz -C /tmp/premake
-export PATH=$PATH:/tmp/premake
-ls -al /tmp/premake
-ls -al .
